9f9f8691e5230c08e0da7b593bde8098948a1daa
CIRA2606 <101607815+CIRA26@users.noreply.github.com>
2022-05-06 12:44:11 -0500

    Add files via upload

---
+++ lib/incl.h
@@ -0,0 +1,20 @@
+/*
+ * include.h
+ *
+
+ */
+
+#ifndef INCLUDE_H_
+#define INCLUDE_H_
+
+//#define GPIO_PORTF_DATA_R       (*((volatile uint32_t *)0x400253FC))
+#include <stdint.h>
+#include <stdlib.h>
+#include "NVIC/NVIC.h"
+#include "SYSCTL/SYSCTL.h"
+#include "GPIO/GPIO.h"
+
+
+
+
+#endif /* INCLUDE_H_ */
